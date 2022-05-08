8b7f610771771c88dd7b99237006ba4a219f1d47
MarinaBohunenko <m.bogunenko@gmail.com>
2022-05-08 18:43:46 +0300

    Five Hello

--- src/main/java/com/marina/Main.java
+++ src/main/java/com/marina/Main.java
@@ -6,5 +6,7 @@ public class Main {
         System.out.println("Hello");
         System.out.println("Hello");
         System.out.println("Hello");
+        System.out.println("Hello");
+        System.out.println("Hello");
     }
 }
